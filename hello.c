317444a2d8a90cc9bbd26e9f320807a28463d8a8
chunter24 <chunter19@huskers.unl.edu>
2020-08-25 00:05:39 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Carrell Hunter
+ * Date: August 18 2020
+ *
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Carrell Hunter!\n");
+     printf("Computer Science\n");
+
+     return 0;
+ }
